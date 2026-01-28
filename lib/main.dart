fbe038d4a0ca0038c27bd3320ec5e61127c2986e
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2026-01-27 21:42:04 -0400

    add: ProviderScope for riverpod use, themeMode use ThemePovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tasks_todo/core/theme/app_theme.dart';
 import 'package:tasks_todo/features/authentication/presentation/pages/login_page.dart';
+import 'package:tasks_todo/features/authentication/presentation/providers/theme_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
+      themeMode: themeMode,
       home: LoginPage(),
     );
   }
