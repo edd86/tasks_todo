67d127ec07897e88a9d79806e5d0b7d06a89758e
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2026-02-03 21:14:50 -0400

    feat: Implement the initial `HomePage` widget displaying a header, calendar strip, task list, and an add task button.

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tasks_todo/features/home/presentation/widgets/lists_task.dart';
 import '../widgets/custom_header.dart';
 import '../widgets/calendar_strip.dart';
-import '../widgets/section_header.dart';
-import '../widgets/task_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -31,43 +30,14 @@ class HomePage extends StatelessWidget {
           ),
           Positioned.fill(
             top: 260, // Start below the calendar
-            child: ListView(
-              padding: const EdgeInsets.only(bottom: 100, top: 20),
-              children: const [
-                SectionHeader(title: "MORNING"),
-                TaskCard(
-                  title: "Morning Routine",
-                  time: "08:00 AM",
-                  category: "HABIT",
-                  isCompleted: true,
-                  categoryColor: Colors.green,
-                ),
-                TaskCard(
-                  title: "Design Sync",
-                  time: "10:30 AM",
-                  category: "WORK",
-                  isCompleted: false,
-                  categoryColor: Colors.blue,
-                ),
-                SectionHeader(title: "AFTERNOON"),
-                TaskCard(
-                  title: "Deep Work Session",
-                  time: "02:00 PM",
-                  category: "FOCUS",
-                  isCompleted: false,
-                  categoryColor: Color(0xFFC3662C),
-                ),
-                TaskCard(
-                  title: "Grocery Shopping",
-                  time: "05:00 PM",
-                  category: "PERSONAL",
-                  isCompleted: false,
-                  categoryColor: Colors.pink,
-                ),
-              ],
-            ),
+            child: ListsTask(),
           ),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_task),
+        onPressed: () {},
       ),
     );
   }
