af930e4fdf86e82c22b37887759ff8c3b77e0b89
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2026-02-05 02:10:56 -0400

    feat: Initialize Supabase and load environment variables in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tasks_todo/core/routes/app_router.dart';
 import 'package:tasks_todo/core/theme/app_theme.dart';
 import 'package:tasks_todo/features/authentication/presentation/providers/theme_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const ProviderScope(child: MainApp()));
 }
 
